8794106873f4e1ead9a3f4a0a26175353cd5e8b8
Meaghan Neylon <mneylon@richmond.edu>
2020-10-11 12:20:13 -0400

    Updated Javascript with Scroll Reveal for animation on page.

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.headline')
+ScrollReveal().reveal('.tagline', { delay: 500 })
+ScrollReveal().reveal('#contact', { delay: 1000 })
+ScrollReveal().reveal('#work', { delay: 1500 })
+ScrollReveal().reveal('#aboutme', { delay: 2000 })
