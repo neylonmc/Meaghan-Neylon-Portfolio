6e32d2f845b38197bcd399f9e9c9a7d29c71d1d2
Meaghan Neylon <mneylon@richmond.edu>
2020-10-11 15:29:44 -0400

    Updated readme file and edited unnecessary code.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# MeaghanNeylon_Portfolio
+# Meaghan Neylon Portfolio
 Portfolio to market my work, contact information and information about myself. 
+
+<img src="images/2020-10-11 14.46.38.gif" alt="GIF of home page">
+
+## About My Portfolio 
+
+My portfolio has pages that allow users to contact me, know a little about me and see some of the projects I have been working on. 
+
+<img src="images/2020-10-11 15.26.23.gif" alt="GIF of webpage">
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ ScrollReveal().reveal('#work', { delay: 2000 })
 
 //Contact HTML 
 ScrollReveal().reveal('.contact')
-ScrollReveal().reveal('.fa-github', { delay: 500 })
+ScrollReveal().reveal('.fa-github-square', { delay: 500 })
 ScrollReveal().reveal('.fa-twitter', { delay: 1000 })
 ScrollReveal().reveal('.fa-linkedin', { delay: 1500 })
 ScrollReveal().reveal('.fa-at', { delay: 2000 })
@@ -18,4 +18,4 @@ ScrollReveal().reveal('.frame', { delay: 500 })
 
 //Portfolio HTML
 ScrollReveal().reveal('.portfolioHeadLine')
-ScrollReveal().reveal('.tagline', { delay: 500 })
+ScrollReveal().reveal('.taglinePortfolio', { delay: 500 })
