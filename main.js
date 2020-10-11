ab4632238625e794e4ea0f78bb499c64115523e6
Meaghan Neylon <mneylon@richmond.edu>
2020-10-11 12:49:52 -0400

    Updated JS with Scroll Reveal

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+//Index HTML
 ScrollReveal().reveal('.headline')
 ScrollReveal().reveal('.tagline', { delay: 500 })
 ScrollReveal().reveal('#contact', { delay: 1000 })
 ScrollReveal().reveal('#work', { delay: 1500 })
 ScrollReveal().reveal('#aboutme', { delay: 2000 })
+
+//Contact HTML 
+ScrollReveal().reveal('.contact')
+ScrollReveal().reveal('.fa-github', { delay: 500 })
+ScrollReveal().reveal('.fa-twitter', { delay: 1000 })
+ScrollReveal().reveal('.fa-linkedin', { delay: 1500 })
+ScrollReveal().reveal('.fa-at', { delay: 2000 })
+
