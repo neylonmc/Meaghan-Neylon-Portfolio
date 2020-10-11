cd55b5947b4ce9d002e85ae5bab0955f2b96e3cb
Meaghan Neylon <mneylon@richmond.edu>
2020-10-11 16:54:28 -0400

    Added resume and resume PDF files.

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ ScrollReveal().reveal('.fa-github-square', { delay: 500 })
 ScrollReveal().reveal('.fa-twitter', { delay: 1000 })
 ScrollReveal().reveal('.fa-linkedin', { delay: 1500 })
 ScrollReveal().reveal('.fa-at', { delay: 2000 })
+ScrollReveal().reveal('.resume', { delay: 2500 })
+
 
 //About Me HTML
 ScrollReveal().reveal('h1')
